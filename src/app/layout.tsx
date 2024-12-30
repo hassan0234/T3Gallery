2ebcc09e6f47fbf41b0324dd7149b066089959a3
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-12-30 22:44:49 +0500

    Database Setup Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,15 @@ export const metadata: Metadata = {
   description: "Created with Learning and love!",
 };
 
+function TopNav() {
+  return (
+    <div className="flex justify-between items-center w-full p-4 border-b">
+      <h1 className="text-xl font-semibold">T3-Gallery</h1>
+      <h1 className="text-xl font-semibold">Sign In</h1>
+    </div>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,8 +34,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col gap-4`}
       >
+        <TopNav />
         {children}
       </body>
     </html>
